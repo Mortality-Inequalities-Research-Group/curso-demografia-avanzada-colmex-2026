7656b82edac82c49d29e488787dda3497aed0686
Jose Manuel Aburto <jm.aburto01@gmail.com>
2026-07-26 14:31:01 -0600

    Hide lab code downloads until course end

--- code/dia-02/yll.R
+++
@@ -1,8 +0,0 @@
-# Run this script from the repository root.
-
-# Load data for Saudi Arabian females.
-load("data/dia-02/yll-data.RData")
-
-#the COD1 object contains the cause of death structure by age group for 2005-2010
-#the LT1 object is the UN lifetables for females in Saudi Arabia in 2005-2010
-source("code/dia-02/yll-functions.R")
